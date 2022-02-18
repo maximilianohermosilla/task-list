a9c864185ade07f1cee2c4e5848fdc584f4a8f81
maximiliano hermosilla <maximiliano_hermosilla@hotmail.com>
2022-02-18 00:11:15 -0300

    Task list

---
+++ src/app/components/mock-tasks.ts
@@ -0,0 +1,28 @@
+import {Task} from "./task"
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: "Terminar primer modulo de Angular",
+        day: "Agosto 5 a las 12:00",
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "Hacer las compras para la cena",
+        day: "Agosto 5 a las 17:00",
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "Investigar sobre Bootstrap",
+        day: "Agosto 5 a las 19:00",
+        reminder: true
+    },
+    {
+        id: 4,
+        text: "Nueva Tarea",
+        day: "Hoy",
+        reminder: true
+    }
+]
