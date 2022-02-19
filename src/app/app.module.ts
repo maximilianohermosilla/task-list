3fe6586862bb95a583a5f82d6f4411ab2c0dde0f
maximiliano hermosilla <maximiliano_hermosilla@hotmail.com>
2022-02-18 23:12:34 -0300

    Finalizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,13 @@ import { TasksItemComponent } from './components/tasks-item/tasks-item.component
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AddTaskComponent } from './components/add-task/add-task.component';
 import { FormsModule } from '@angular/forms'
-import { RouterModule, Routes} from '@angular/router'
+import { RouterModule, Routes} from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/footer/footer.component'
 
 const appRoutes:Routes = [
-  {path: '', component: TasksComponent}
+  {path: '', component: TasksComponent},
+  {path: 'about', component: AboutComponent}
 ]
 
 @NgModule({
@@ -22,7 +25,9 @@ const appRoutes:Routes = [
     ButtonComponent,
     TasksComponent,
     TasksItemComponent,
-    AddTaskComponent
+    AddTaskComponent,
+    AboutComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UiService } from 'src/app/services/ui.service'
 import { Subscription } from 'rxjs'
-
+import { Router } from '@angular/router'
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -14,7 +14,8 @@ export class HeaderComponent implements OnInit {
   subscription?: Subscription;
 
   constructor(
-      private uiService:UiService
+      private uiService:UiService,
+      private router: Router
   ) {
       this.subscription = this.uiService.onToggle().subscribe(
         value => this.showAddTask = value
@@ -26,6 +27,10 @@ export class HeaderComponent implements OnInit {
 
   toggleAddTask(){
     this.uiService.toggleAddTask();
+  }
+
+  hasRoute(route: string){
+    return this.router.url === route;
   }
 
 }
